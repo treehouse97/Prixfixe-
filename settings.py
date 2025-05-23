618735ef478428aadd38bff0af4b0e011c121ad4
treehouse97 <wariest-pavlova2c@icloud.com>
2025-05-22 23:42:29 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 
 GOOGLE_API_KEY = "AIzaSyApX2q-0DaM5xqJGGyiyFA6gkRe7rRxaeM"  # <-- Paste your Google Maps API key here
-DEFAULT_LOCATION = "40.7891,-73.1350"  # Central Long Island
-SEARCH_RADIUS_METERS = 5000
+DEFAULT_LOCATION = "40.7268,-73.6348"  # Garden City, NY
+SEARCH_RADIUS_METERS = 10000
