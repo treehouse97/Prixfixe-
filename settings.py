75572313e669c87426b9f64ee117d2218c761abe
treehouse97 <wariest-pavlova2c@icloud.com>
2025-05-22 23:33:37 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 
 GOOGLE_API_KEY = "AIzaSyApX2q-0DaM5xqJGGyiyFA6gkRe7rRxaeM"  # <-- Paste your Google Maps API key here
-DEFAULT_LOCATION = "40.7128,-74.0060"  # NYC lat/lng
+DEFAULT_LOCATION = "40.7891,-73.1350"  # Central Long Island
 SEARCH_RADIUS_METERS = 5000
