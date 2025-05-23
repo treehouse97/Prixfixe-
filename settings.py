0c3b8fa7617bff534f8747438a569fb7e5c85d8b
treehouse97 <wariest-pavlova2c@icloud.com>
2025-05-22 23:32:42 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 
 GOOGLE_API_KEY = "AIzaSyApX2q-0DaM5xqJGGyiyFA6gkRe7rRxaeM"  # <-- Paste your Google Maps API key here
-DEFAULT_LOCATION = "New York, NY"
+DEFAULT_LOCATION = "40.7128,-74.0060"  # NYC lat/lng
 SEARCH_RADIUS_METERS = 5000
