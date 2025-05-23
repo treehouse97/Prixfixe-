4ad08b15b5b666508d9f6b21b2c6122e4e194b93
treehouse97 <wariest-pavlova2c@icloud.com>
2025-05-23 01:45:30 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 
 GOOGLE_API_KEY = "AIzaSyApX2q-0DaM5xqJGGyiyFA6gkRe7rRxaeM"  # <-- Paste your Google Maps API key here
-DEFAULT_LOCATION = "40.7297,-73.2104"  # Islip, NY
+DEFAULT_LOCATION = "40.7268,-73.6348"  # Garden City, NY
 SEARCH_RADIUS_METERS = 20000
