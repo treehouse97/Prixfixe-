8baebd936f22b080c11b9527fd2144dac3ac3cd7
treehouse97 <wariest-pavlova2c@icloud.com>
2025-05-22 23:39:23 -0400

    Update places_api.py

--- places_api.py
+++ places_api.py
@@ -1,24 +1,25 @@
-
 import requests
 from settings import GOOGLE_API_KEY
 
-PLACES_ENDPOINT = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
-GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json'
-
-def find_restaurants(location='New York, NY', radius=5000, keyword='restaurant'):
-    geo_response = requests.get(GEOCODE_ENDPOINT, params={'address': location, 'key': GOOGLE_API_KEY}).json()
-    if geo_response['status'] != 'OK':
-        print('Geocoding failed:', geo_response)
-        return []
-
-    latlng = geo_response['results'][0]['geometry']['location']
+def find_restaurants(location, radius):
+    endpoint = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
     params = {
-        'location': f"{latlng['lat']},{latlng['lng']}",
-        'radius': radius,
-        'keyword': keyword,
-        'type': 'restaurant',
-        'key': GOOGLE_API_KEY
+        "location": location,
+        "radius": radius,
+        "type": "restaurant",
+        "key": GOOGLE_API_KEY
     }
 
-    response = requests.get(PLACES_ENDPOINT, params=params).json()
-    return response.get('results', [])
+    response = requests.get(endpoint, params=params)
+    try:
+        data = response.json()
+    except Exception as e:
+        print("Failed to parse response:", e)
+        return []
+
+    if "results" not in data:
+        print("Google API error:", data)
+        return []
+
+    print(f"Google returned {len(data['results'])} places")
+    return data['results']
